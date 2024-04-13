dce25b524a14ca94935a649c084471f1648887bb
Alexey Morozov <alexey.morozov@live.ru>
2024-04-13 13:57:43 +0300

    Output URL when serving examples locally

--- webpackServe.js
+++ webpackServe.js
@@ -10,6 +10,7 @@ const configurations = examplesConfiguration(
   process.argv
 );
 
+const SERVE_PORT = process.env.SERVE_PORT ? Number(process.env.SERVE_PORT) : 10555;
 const SPARQL_ENDPOINT = process.env.SPARQL_ENDPOINT;
 const WIKIDATA_ENDPOINT = process.env.WIKIDATA_ENDPOINT
   ?? 'https://query.wikidata.org/sparql';
@@ -40,4 +41,6 @@ app.use(createProxyMiddleware('/wikidata**', {
   secure: false,
 }));
 
-const server = app.listen(10555);
+// eslint-disable-next-line no-console
+console.log(`Running Webpack server at http://localhost:${SERVE_PORT}`);
+const server = app.listen(SERVE_PORT, 'localhost');
