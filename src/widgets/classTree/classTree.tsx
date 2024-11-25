aaf88d21c630236cf804d8b52d3fd24744595707
Alexey Morozov <alexey.morozov@live.ru>
2024-11-26 00:18:03 +0300

    Fix missing count badges in `ClassTree`, use only `knownElementTypes()` data when displaying the tree (#24)

--- src/widgets/classTree/classTree.tsx
+++ src/widgets/classTree/classTree.tsx
@@ -168,11 +168,7 @@ class ClassTreeInner extends React.Component<ClassTreeInnerProps, State> {
         this.listener.listen(model.events, 'loadingStart', () => {
             this.initClassTree();
         });
-        this.listener.listen(model.events, 'elementTypeEvent', ({data}) => {
-            if (data.changeData) {
-                this.delayedClassUpdate.call(this.refreshClassTree);
-            }
-        });
+
         this.initClassTree();
     }
 
@@ -425,14 +421,10 @@ function createRoots(
     const mapChildren = (children: Iterable<ClassTreeItem>): TreeNode[] => {
         const nodes: TreeNode[] = [];
         for (const item of children) {
-            const data = model.getElementType(item.id)?.data;
             nodes.push({
                 iri: item.id,
-                data,
-                label: model.locale.formatLabel(
-                    item.label.length === 0 ? data?.label : item.label,
-                    item.id
-                ),
+                data: item,
+                label: model.locale.formatLabel(item.label, item.id),
                 derived: mapChildren(item.children),
             });
         }
