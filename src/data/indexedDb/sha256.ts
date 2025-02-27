70c2776d7c184cbf5fc7ea663fc5a1736e60490e
Alexey Morozov <alexey.morozov@live.ru>
2025-02-28 00:05:10 +0300

    Move `sha256.ts` third-party license text to `THIRDPARTY.md`

--- src/data/indexedDb/sha256.ts
+++ src/data/indexedDb/sha256.ts
@@ -1,34 +1,15 @@
-/*!
+/*
  * SHA-256 implementation adopted from https://github.com/brillout/forge-sha256
- *
  * Commit: 6ad5535e0be2385fdc53f1d9ce2b172365c70333
  * Date: Jul 5, 2017
+ *
+ * The MIT License (MIT)
+ * Copyright (c) 2015-2017 Romuald Brillout and contributors
+ * (See THIRDPARTY.md for the full license text.)
+ *
+ * With modifications (static typing and replaced buffer implementation)
+ * made under LGPL-2.1-or-later (see NOTICE.md).
  */
-
-/*!
-The MIT License (MIT)
-
-Copyright (c) 2015-2017 Romuald Brillout and contributors
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-*/
-
 import { CircularBuffer } from './circularBuffer';
 
 export class Sha256 {
