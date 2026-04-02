2ac8e466b33a61b124b1baf635455e2712665862
Alexey Morozov <alexey.morozov@live.ru>
2026-04-02 23:32:54 +0300

    Refactor `ConnectionsMenu` into multiple sub-components

---
+++ src/widgets/connectionsMenu/index.ts
@@ -0,0 +1,6 @@
+export {
+    ConnectionsMenu, type ConnectionsMenuProps, type ConnectionsMenuCommands,
+} from './connectionsMenu';
+export type {
+    PropertySuggestionHandler, PropertySuggestionParams, PropertyScore,
+} from './menuCommon';
